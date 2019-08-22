cc4baf7d6d4bf8e995ed6b5db8cb5676a73fa5e7
Janis Kikans <janis.kikans@gmail.com>
2019-08-22 15:17:47 +0300

    refactor functions.php

--- src/functions.php
+++ src/functions.php
@@ -3,7 +3,7 @@
 /**
  * Redirect helper function.
  */
-if (!function_exists('redirect')) {
+if ( ! function_exists('redirect')) {
     function redirect(string $target = '/')
     {
         header("Location: $target");
